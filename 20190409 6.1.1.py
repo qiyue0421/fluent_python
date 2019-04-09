3a246d3bb92a1efa3991cee481d5b6548265f381
lurenqiyue <1831545391@qq.com>
2019-04-09 21:38:37 +0800

    “策略”模式的实现

---
+++ 20190409 6.1.1.py
@@ -0,0 +1,135 @@
+# 经典“策略”模式的实现
+"""
+假如一个网店指定了下列折扣规则：
+1、有1000或以上积分的顾客，每个订单享5%折扣
+2、同一订单中，单个商品的数量达到20个或以上，享10%折扣
+3、订单中的不同商品达到10个或以上，享7%折扣
+"""
+from abc import ABC, abstractmethod
+from collections import namedtuple
+
+# 客户类，命名元组接受两个参数：类名、字段名
+Customer = namedtuple('Customer', 'name fidelity')
+
+
+# 商品类
+class LineItem:
+    def __init__(self, product, quantity, price):
+        self.product = product
+        self.quantity = quantity
+        self.price = price
+
+    def total(self):
+        return self.quantity * self.price
+
+
+# 上下文
+# 购物类
+class Order:
+    def __init__(self, customer, cart, promotion=None):
+        self.customer = customer
+        # 订单列表
+        self.cart = list(cart)
+        self.promotion = promotion
+
+    # 订单总价
+    def total(self):
+        if not hasattr(self, '__total'):
+            self.__total = sum(item.total() for item in self.cart)
+        return self.__total
+
+    # 订单减去折扣
+    def due(self):
+        if self.promotion is None:
+            discount = 0
+        else:
+            discount = self.promotion.discount(self)
+        return self.total() - discount
+
+    def __repr__(self):
+        fmt = '<Order total:{:2f} due: {:2f}>'
+        return fmt.format(self.total(), self.due())
+
+
+# 策略
+# 将Promotion定义为抽象基类，这么做是为了使用@abstractmethod装饰器，从而明确表明所用的模式
+# 声明抽象基类最简单的方式是子类化 abc.ABC
+class Promotion(ABC):
+    @abstractmethod
+    def discount(self, order):
+        """返回折扣金额（正值）"""
+
+
+# 第一个具体策略
+# 客户积分大于1000分可以享受折扣
+class FidelityPromo(Promotion):
+    def discount(self, order):
+        return order.total() * .05 if order.customer.fidelity >= 1000 else 0
+
+
+# 第二个具体策略
+# 订单中单个商品的个数大于20可以享受折扣
+class BulkItemPromo(Promotion):
+    def discount(self, order):
+        discount = 0
+        # 遍历订单列表中的每个商品
+        for item in order.cart:
+            # 检索数量大于20的商品
+            if item.quantity >= 20:
+                discount += item.total() * .1
+        return discount
+
+
+# 第三个具体策略
+# 订单中不同商品达到10个或以上，享受折扣
+class LargeOrderPromo(Promotion):
+    def discount(self, order):
+        # 利用集合推导式获取订单中不同商品的列表
+        distinct_items = {item.product for item in order.cart}
+        if len(distinct_items) >= 10:
+            return order.total() * .07
+
+
+# 实例化客户joe，积分为0
+joe = Customer('John Doe', 0)
+# 实例化客户ann，积分为1000
+ann = Customer('Ann Smith', 1000)
+
+# 创建订单列表，通过实例化商品类构建
+cart = [LineItem('banana', 4, .5), LineItem('apple', 10, 1.5), LineItem('watermelon', 5, 5.0)]
+# 构建单个商品数量大于20的订单
+banana_cart = [LineItem('banana', 30, .5), LineItem('apple', 10, 1.5)]
+# 构建包含10个不同商品的订单
+larger_cart = [LineItem(str(item), 1, 1.0) for item in range(10)]
+
+# 实例化一次消费，客户joe，折扣策略为根据积分计算
+print(Order(joe, cart, FidelityPromo()))
+"""
+结果为
+<Order total:42.000000 due: 42.000000>
+因为该客户的积分为0，不享受折扣
+"""
+
+# 实例化一次消费，客户ann，折扣策略为第一个策略
+print(Order(ann, cart, FidelityPromo()))
+"""
+结果为
+<Order total:42.000000 due: 39.900000>
+因为该客户的积分为1000
+"""
+
+# 来看看第二个策略是否可行
+print(Order(joe, banana_cart, BulkItemPromo()))
+"""
+结果为
+<Order total:30.000000 due: 28.500000>
+该订单中香蕉的数量已经超过了20
+"""
+
+# 来看看第三个策略是否可行
+print(Order(joe, larger_cart, LargeOrderPromo()))
+"""
+结果为
+<Order total:10.000000 due: 9.300000>
+该订单中有10种不同的商品
+"""
