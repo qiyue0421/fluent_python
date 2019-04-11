285f1adeadea5841669f2d93ef3569214d4fdd8a
lurenqiyue <1831545391@qq.com>
2019-04-11 19:58:05 +0800

    一个简单的装饰器的实现

---
+++ 20190411 7.7.py
@@ -0,0 +1,31 @@
+# 一个简单的装饰器，输出函数的运行时间
+import time
+
+
+def clock(func):
+    def clocked(*args):
+        t0 = time.perf_counter()
+        result = func(*args)
+        elapsed = time.perf_counter() - t0
+        name = func.__name__
+        arg_str = ','.join(repr(arg) for arg in args)
+        print('[%0.7fs] %s(%s) -> %r' % (elapsed, name, arg_str, result))
+        return result
+    return clocked
+
+
+@clock
+def snooze(seconds):
+    time.sleep(seconds)
+
+
+@clock   # 等价于 factorial = clock(factorial)
+def factorial(n):
+    return 1 if n < 2 else n*factorial(n-1)
+
+
+if __name__ == '__main__':
+    print('*' * 40, 'Calling snooze(.123)')
+    snooze(.123)
+    print('*' * 40, 'Calling factorial(6)')
+    print('6! =', factorial(6))
