4b2fa692f93ccf5278eb00a115f6e9e996e4fd2e
lurenqiyue <1831545391@qq.com>
2019-04-19 11:40:14 +0800

    向量类Vector2d的实现——魔术方法-改进版

--- 20190419 9.2.py
+++ 20190419 9.2.py
@@ -7,8 +7,16 @@ class Vector2d:
     typecode = 'd'
 
     def __init__(self, x, y):
-        self.x = float(x)  # 转换为浮点型
-        self.y = float(y)
+        self.__x = float(x)  # 转换为浮点型
+        self.__y = float(y)
+
+    @property   # 将读值方法标记为特性
+    def x(self):
+        return self.__x
+
+    @property
+    def y(self):
+        return self.__y
 
     # 将实例变成可迭代对象，实现拆包功能
     def __iter__(self):
@@ -23,6 +31,7 @@ class Vector2d:
     def __str__(self):
         return str(tuple(self))
 
+    # 转换为字节
     def __bytes__(self):
         return (bytes([ord(self.typecode)]) + bytes(array(self.typecode, self)))
 
@@ -36,6 +45,27 @@ class Vector2d:
     def __bool__(self):
         return bool(abs(self))
 
+    # 使实例变成可散列的需要实现hash
+    def __hash__(self):
+        return hash(self.x) ^ hash(self.y)
+
+    # 计算角度
+    def angle(self):
+        return math.atan2(self.y, self.x)
+
+    # 格式化输出
+    def __format__(self, format_spec=''):
+        if format_spec.endswith('p'):        # 极坐标表示
+            format_spec = format_spec[:-1]  # 删除'p'后缀
+            coords = (abs(self), self.angle())
+            outer_fmt = '<{}, {}>'
+        else:       # 直角坐标表示
+            coords = self
+            outer_fmt = '({}. {})'
+        # 将format_spec应用到向量的各个分量上，构建一个可迭代的格式化字符
+        components = (format(c, format_spec) for c in coords)
+        return outer_fmt.format(*components)  # 将格式化字符串带入要输出的格式中
+
 
 v1 = Vector2d(3, 4)
 print(v1.x, v1.y)
@@ -43,6 +73,4 @@ print(v1.x, v1.y)
 x, y = v1
 print(x, y)
 
-v1
 print(v1)
-
